6d0b6891470a8387efe50bfb30e11c0e989cc2c0
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-06-15 18:11:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+  const cleaned = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  
+  // Check if the cleaned string is equal to its reverse
+  const reversed = cleaned.split('').reverse().join('');
+  
+  return cleaned === reversed;
 
 }
 module.exports = palindrome
